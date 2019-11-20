dc4916fffe9119321c8195020222290e5bb05dfc
Denis Yakimov <55536597+DenisYakimov@users.noreply.github.com>
2019-11-20 21:35:05 +0300

    factorial
    Факториал

---
+++ math/factorial.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+/*
+Факториал
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(reader.readLine());
+        reader.close();
+
+        System.out.println(factorial(input));
+    }
+
+    public static String factorial(int n) {
+        if (n < 0) {
+            return "0";
+        } 
+        else if (n == 0) {
+            return "1";
+            
+        }else {
+            BigInteger big = new BigInteger("1");
+            for (int i = 1; i <= n; i++ ) {
+                big = big.multiply(BigInteger.valueOf(i));
+                }
+            return big.toString();
+        }
+    }
+}
